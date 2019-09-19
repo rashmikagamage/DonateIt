f431b838b749f5f6d43844003f589330e2cbc10d
Dn <53206377+Dn98@users.noreply.github.com>
2019-09-19 13:00:44 +0530

    Add files via upload

---
+++ DonateIt/app/src/main/java/com/example/student/donateit/DonateOrgans.java
@@ -0,0 +1,13 @@
+package com.example.student.donateit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonateOrgans extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate_organs);
+    }
+}
