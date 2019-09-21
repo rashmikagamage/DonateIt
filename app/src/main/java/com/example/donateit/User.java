1006fdc9e10825714551822b19a1fe2c650bd9c7
rashmika madushan <rashmikamadushan321@gmail.com>
2019-09-21 10:34:47 +0530

    Donate it

---
+++ app/src/main/java/com/example/donateit/User.java
@@ -0,0 +1,62 @@
+package com.example.donateit;
+
+import java.util.Date;
+
+public class User {
+
+    String email;
+     String name;
+   String location;
+    String accountType;
+     String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
