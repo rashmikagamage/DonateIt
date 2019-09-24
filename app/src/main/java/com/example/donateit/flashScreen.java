ba8d7421c2cfaa264ec2bfe51580208c3372e3af
rashmika madushan <rashmikamadushan321@gmail.com>
2019-09-24 20:53:35 +0530

    commit

--- app/src/main/java/com/example/donateit/flashScreen.java
+++ app/src/main/java/com/example/donateit/flashScreen.java
@@ -1,5 +1,7 @@
 package com.example.donateit;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,19 @@ public class flashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flash_screen);
 
-        //
+Handler handler;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flash_screeen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(flashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
